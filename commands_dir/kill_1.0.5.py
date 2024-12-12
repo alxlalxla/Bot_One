879a67b0c8f3da275f812f716b68cf118370e6ea
alxlalxla <107724467+alxlalxla@users.noreply.github.com>
2024-12-12 16:14:30 +0100

    Update kill_1.0.5.py

--- commands_dir/kill_1.0.5.py
+++ commands_dir/kill_1.0.5.py
@@ -8,11 +8,8 @@ description = "termina un'applicazione"
 enable_access_control = True
 access_denied_message = "{ctx.author.name} non sei autorizzato ad eseguire questo comando!"
 app_denied_message = "{ctx.author.name} operazione non consentita."
-# Messaggio che elenca le applicazioni killabili
 killable_apps_message = "{ctx.author.name}, le app killabili sono: {apps}."
-# Messaggio di successo per la terminazione dell'applicazione
 app_terminated_message = "{app} è stato terminato."
-# Lista di applicazioni che possono essere terminate
 killable_apps = ["mpv", "espeak-ng"]
 
 with open("config.json", "r") as file:
@@ -37,18 +34,12 @@ class Bot(commands.Bot):
         if enable_access_control and ctx.author.name.lower() not in enabled_users:
             await ctx.send(access_denied_message.format(ctx=ctx))
             return
-
-        # Se l'app è None, invia il messaggio con le app killabili
         if app is None:
             await ctx.send(killable_apps_message.format(ctx=ctx, apps=', '.join(killable_apps)))
             return
-
-        # Controlla se l'app è nella lista delle app killabili
         if app.lower() not in killable_apps:
             await ctx.send(app_denied_message.format(ctx=ctx))
             return
-
-        # Esegui il comando pkill per terminare l'applicazione
         subprocess.run(["pkill", app.lower()])
         await ctx.send(app_terminated_message.format(app=app))
 
